3a641e8b4224ad6aa1b3540c5e4ffe356338f347
하현수 <dhy03196@naver.com>
2020-12-09 19:11:49 +0900

    [ADD] JavaScript for Upload Video

---
+++ app/static/js/upload.js
@@ -0,0 +1,62 @@
+document.querySelectorAll(".drop-zone__input").forEach(inputElement => {
+    const dropZoneElement = inputElement.closest(".drop-zone");
+
+    dropZoneElement.addEventListener("click", e => {
+        inputElement.click();
+    });
+
+    inputElement.addEventListener("change", e => {
+        if (inputElement.files.length) {
+            updateThumbnail(dropZoneElement, inputElement.files[0]);
+        }
+    });
+
+    dropZoneElement.addEventListener("dragover", e => {
+        e.preventDefault();
+        dropZoneElement.classList.add("drop-zone--over");
+    });
+
+    ["dragleave", "dragend"].forEach(type => {
+        dropZoneElement.addEventListener(type, e => {
+            dropZoneElement.classList.remove("drop-zone--over");
+        });
+    });
+
+    dropZoneElement.addEventListener('drop', e => {
+        e.preventDefault();
+
+        if (e.dataTransfer.files.length) {
+            inputElement.files = e.dataTransfer.files;
+            updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+        }
+
+        dropZoneElement.classList.remove('drop-zone--over'); 
+    });
+
+});
+
+function updateThumbnail(dropZoneElement, file){
+    let thumbnailElement = dropZoneElement.querySelector('.drop-zone__thumb');
+
+    if (dropZoneElement.querySelector('.drop-zone__prompt')) {
+        dropZoneElement.querySelector('.drop-zone__prompt').remove();
+    }
+    
+    if (!thumbnailElement){
+        thumbnailElement = document.createElement('div');
+        thumbnailElement.classList.add('drop-zone__thumb');
+        dropZoneElement.appendChild(thumbnailElement);
+    }
+
+    thumbnailElement.dataset.label = file.name;
+
+    if (file.type.startsWith('video/')) {
+        thumbnailElement.style.backgroundImage = "url(/static/img/video_icon.png)";
+        
+        const  fileReader = new FileReader();
+        fileReader.readAsArrayBuffer(file);
+        console.log(file);
+        fileReader.onload = () => {
+        };
+    }
+}
